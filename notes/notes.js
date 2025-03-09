0f68931a45900864228e4ba8ec893ef6243a7967
unknown <akashgite550@gmail.com>
2025-03-10 00:19:47 +0530

    backend working

---
+++ notes/notes.js
@@ -0,0 +1,19 @@
+// starting a backend project 
+// npm init -y for importing package.json file npm is node package manager 
+// what is packge manager and its use 
+//A package manager is a tool that helps you install, update, and manage software libraries (packages) easily.
+
+
+
+//! Real-life Example
+// Imagine you're setting up a new phone:
+//     App Store/Play Store = Package Manager
+//     Apps = Packages (e.g., WhatsApp, Instagram)
+// Instead of downloading apps from random websites, you use the App Store to install, update, or delete apps safely. The App Store also ensures the apps are compatible with your phone.
+// In Programming:
+//     npm (JavaScript)
+//     pip (Python)
+//     composer (PHP)
+//     apt (Linux)
+
+// 
