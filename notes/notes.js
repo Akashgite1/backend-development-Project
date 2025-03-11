12e21cd6144beefec26753a47861bbd64d042766
unknown <akashgite550@gmail.com>
2025-03-11 14:25:44 +0530

    add updated files

--- notes/notes.js
+++ notes/notes.js
@@ -16,4 +16,26 @@
 //     composer (PHP)
 //     apt (Linux)
 
-// 
+// made the folder + file 
+//  public/temp
+
+// .gitkeep file is used to keep the folder in the git repo even if it is empty 
+// since the git does not keep the empty folder in the repo 
+
+//.gitignore file is used to ignore the files that we do not want to push to the git repo 
+// for example node_modules folder is not pushed to the git repo 
+// so we add node_modules in the .gitignore file 
+
+// env file is used to keep the secret keys and passwords 
+// we do not want to push the secret keys and passwords to the git repo 
+
+// src file is used to keep the source code of the project 
+// made the src folder 
+
+// nodemon is used to run the server when we make changes in the code nodemon automatically restarts the server so we do not have to restart the server again and again  
+// npm install nodemon --save-dev 
+
+//! mkdir is used to make the folder 
+// mkdir foldername 
+// example mkdir src database
+
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # backend development 
+
+ this project made for development and learning perpose
